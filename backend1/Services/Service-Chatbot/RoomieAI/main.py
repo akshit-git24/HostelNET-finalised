7299eb894be0f2c7ae4ad4a0e33217bc489d8358
Akshit Sahore <akshitsahore2827@gmail.com>
2026-01-14 22:59:46 +0530

    Update main.py

--- backend1/Services/Service-Chatbot/RoomieAI/main.py
+++ backend1/Services/Service-Chatbot/RoomieAI/main.py
@@ -72,5 +72,6 @@ async def read_root(query:str):
     response = client.models.generate_content(
         model="gemini-2.5-flash", contents=prompt
     )
+    
     print(response.text)
     return {"response": response.text}
